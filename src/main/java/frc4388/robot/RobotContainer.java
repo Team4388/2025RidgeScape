a509dc07820d397aafd259d6cbe0329c256b82e7
Michael Mikovsky <77305074+Astatin3@users.noreply.github.com>
2025-01-14 17:43:11 -0700

    imported auto constants

--- src/main/java/frc4388/robot/RobotContainer.java
+++ src/main/java/frc4388/robot/RobotContainer.java
@@ -20,6 +20,7 @@ import frc4388.utility.controller.XboxController;
 import frc4388.utility.controller.DeadbandedXboxController;
 import frc4388.robot.Constants.OIConstants;
 import frc4388.robot.Constants.SwerveDriveConstants;
+import frc4388.robot.Constants.SwerveDriveConstants.AutoConstants;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
