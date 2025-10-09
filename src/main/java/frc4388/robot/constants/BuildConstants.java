9d3290a22d3d762914856191a44a3152c7694b34
Zach Wilke <83995467+C4llSqin@users.noreply.github.com>
2025-10-09 17:51:22 -0600

    Pre-kcmt tuning
    fixed camera pos
    fixed l4 scoring distance

--- src/main/java/frc4388/robot/constants/BuildConstants.java
+++ src/main/java/frc4388/robot/constants/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025RidgeScape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 178;
-  public static final String GIT_SHA = "d437461ec4cc558678b4a78a070424d1aab84acb";
-  public static final String GIT_DATE = "2025-09-30 17:51:59 MDT";
+  public static final int GIT_REVISION = 179;
+  public static final String GIT_SHA = "635defe5502195fe3b7a2d1cf10f6fc3a6f5d6db";
+  public static final String GIT_DATE = "2025-10-02 17:42:14 MDT";
   public static final String GIT_BRANCH = "advantagekit";
-  public static final String BUILD_DATE = "2025-10-02 17:11:15 MDT";
-  public static final long BUILD_UNIX_TIME = 1759446675378L;
+  public static final String BUILD_DATE = "2025-10-09 17:29:09 MDT";
+  public static final long BUILD_UNIX_TIME = 1760052549843L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
--- src/main/java/frc4388/robot/constants/Constants.java
+++ src/main/java/frc4388/robot/constants/Constants.java
@@ -80,7 +80,7 @@ public final class Constants {
         public static final double HALF_ROBOT_SIZE = Units.inchesToMeters(18);
 
         public static final double L4_DISTANCE_PREP = HALF_ROBOT_SIZE + Units.inchesToMeters(15 - 6);
-        public static final double L4_DISTANCE_SCORE = L4_DISTANCE_PREP +  Units.inchesToMeters(-6);
+        public static final double L4_DISTANCE_SCORE = L4_DISTANCE_PREP +  Units.inchesToMeters(-4);
         // public static final double L4_DISTANCE_SCORE = HALF_ROBOT_SIZE + Units.inchesToMeters(4.5);
         
         public static final double L3_DISTANCE_PREP = HALF_ROBOT_SIZE + Units.inchesToMeters(15);
@@ -114,8 +114,8 @@ public final class Constants {
         public static final String LEFT_CAMERA_NAME = "CAMERA_LEFT";
         public static final String RIGHT_CAMERA_NAME = "CAMERA_RIGHT";
 
-        public static final Transform3d LEFT_CAMERA_POS = new Transform3d(new Translation3d(Units.inchesToMeters(5.3), Units.inchesToMeters(-8.75 + 6), Units.inchesToMeters(9)), new Rotation3d(0,0.0,-21.0 * Math.PI / 180));
-        public static final Transform3d RIGHT_CAMERA_POS = new Transform3d(new Translation3d(Units.inchesToMeters(5.3), Units.inchesToMeters(-8.75 + 6), Units.inchesToMeters(9)), new Rotation3d(0,0.0,15.0 * Math.PI / 180));
+        public static final Transform3d LEFT_CAMERA_POS = new Transform3d(new Translation3d(Units.inchesToMeters(5.3 - 1), Units.inchesToMeters(8.75 + 4), Units.inchesToMeters(9)), new Rotation3d(0,0.0,-21.0 * Math.PI / 180));
+        public static final Transform3d RIGHT_CAMERA_POS = new Transform3d(new Translation3d(Units.inchesToMeters(5.3 -2), Units.inchesToMeters(-8.75 + 4), Units.inchesToMeters(9)), new Rotation3d(0,0.0,15.0 * Math.PI / 180));
         
         public static final double MIN_ESTIMATION_DISTANCE = 2; // Meters
 
