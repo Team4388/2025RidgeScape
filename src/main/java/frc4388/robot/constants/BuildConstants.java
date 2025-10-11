4d20c5064279d9e0ac890c93e644e81dbabf929c
Zach Wilke <83995467+C4llSqin@users.noreply.github.com>
2025-10-11 15:24:47 -0600

    commit for robot swithc
    camera angles
    
    l3 and l4 scoring distances

--- src/main/java/frc4388/robot/constants/BuildConstants.java
+++ src/main/java/frc4388/robot/constants/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025RidgeScape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 181;
-  public static final String GIT_SHA = "ba0cfce5774aac2b4e8c86abf532cbc0cac1ee7a";
-  public static final String GIT_DATE = "2025-10-10 18:51:29 MDT";
+  public static final int GIT_REVISION = 182;
+  public static final String GIT_SHA = "88a4a00c29a2e2bdd9c14715807a8fd26499f9cf";
+  public static final String GIT_DATE = "2025-10-10 20:22:54 MDT";
   public static final String GIT_BRANCH = "advantagekit";
-  public static final String BUILD_DATE = "2025-10-10 20:08:21 MDT";
-  public static final long BUILD_UNIX_TIME = 1760148501289L;
+  public static final String BUILD_DATE = "2025-10-11 14:12:35 MDT";
+  public static final long BUILD_UNIX_TIME = 1760213555484L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
